6235eb0d7e47c535dcc55cee6485200c8d4bdc55
Matthew Malensek <matt@malensek.net>
2015-11-28 23:07:03 -0700

    Fix reset escape

--- showcolors.sh
+++ showcolors.sh
@@ -5,14 +5,14 @@ print_strip() {
         inv=$(((color + 8) % 16))
         echo -ne "\x1b[48;5;${color}m \x1b[38;5;${inv}m${3} "
     done
-    echo -e "\e[0m"
+    echo -e "\033[0m"
 }
 
 print_plain() {
     for ((color = ${1}; color < ${2}; ++color)); do
         echo -ne "\x1b[48;5;${color}m ${3} "
     done
-    echo -e "\e[0m"
+    echo -e "\033[0m"
 }
 
 print_strip 0 8
